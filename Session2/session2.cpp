9226aa019ba3b5a6f94e7090022717b7bfc80fd2
ariellucien <ariel.garcia@itesm.mx>
2021-05-14 13:39:27 -0500

    sesión 2

--- Session2/session2.cpp
+++ Session2/session2.cpp
@@ -4,25 +4,47 @@ using namespace std;
 
 class Person
 {
-protected:
+public:
     string name;
     int age;
     Person* parent;
+    int* numeroParaDestructor;
 public:
-    //Person(){}            //default contructor
-    Person() = default;     
-    Person(string, int);    //params constructor
+    Person() : name("generic"), age(50), parent(0), numeroParaDestructor(new int) {}            //default contructor
+    //Person() = default;     
+    Person(string, int, Person*);    //params constructor
     Person(const Person&);  //copy constructor
+    string getName()
+    {
+        return name;
+    }
+
+    ~Person()
+    {
+        cout << "estoy en el destructor";
+        delete numeroParaDestructor;
+    }
 };
 
-Person::Person(string theName, int theAge)
+Person::Person(string name, int age, Person* parent) : name(name), age(age), parent(parent){}
+
+Person::Person(const Person& original)   //Deep Copy
 {
-    name = theName;
-    age = theAge;
+    name = "generico";
+    //age = origin.getAge();
+    parent = new Person("otherName", 40, 0);   // Deep Copy
 }
 
 int main()
 {
-    Person john("john", 75);
+    Person* joseph = new Person("joseph", 40, 0);
+    Person andrew("andrew", 60, 0);
+    Person john("john", 75, &andrew);
     Person dave = john;
+    cout << dave.name;
+    cout << john.parent << endl;
+    cout << dave.parent << endl;
+    cout << dave.parent->name;
+    cout << dave.name;
+    delete joseph;
 }
